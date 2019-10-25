5282118d50908460befe7387a8217848d5605f3e
Ramiro Gisone <ramirogisone@gmail.com>
2019-10-24 23:28:14 -0300

    agrego funciones para creacion de cards

--- js/view.js
+++ js/view.js
@@ -45,18 +45,41 @@ const addCard = () => {
     cardContainer.appendChild(createCards);
 }
 
-const createCards = () => {
-    const card  = createElement('div', '', 'card', '', '');
-    const img   = createElement('img', '', 'card-img-top','', '');
-    const cardB = createElement('div', '', 'card-body', '', '');
-    const title = createElement('h5', '', 'card-title', '', 'Titulo de la card');
-    const text  = createElement('p','','card-text', '', 'Contenido de la card');
-    const textS = createElement('small', '', 'text-muted', '', 'Texto small');
+// elementType, id, className, events, placeholder, content
+const createCard = (content) => {
+    const card  = createElement('div', '', 'card', false, false, '');
+    const img   = createElement('img', '', 'card-img-top',false, false, '');
+    const cardB = createElement('div', '', 'card-body', false, false, '');
+    const title = createElement('h5', '', 'card-title', false, false, content.title);
+    const text  = createElement('p','','card-text', false, false, content.text);
+    const textS = createElement('small', '', 'text-muted', false, false, content.textS);
     card.appendChild(img);
     card.appendChild(cardB);
     card.appendChild(title);
     card.appendChild(text);
     card.appendChild(textS);
 }
+// elementType, id, className, events, placeholder, content
 
+const contentCards = (prod) => {
+    const row  = createElement('div', '', 'row mb-2', false, false, '');
+    const col1 = createElement('div', '', 'col', false, false, '');
+    const col2 = createElement('div', '', 'col', false, false, '');
+    const col3 = createElement('div', '', 'col', false, false, '');
+    const card = createCard(prod);
+    col1 = appendChild(card);
+    col2 = appendChild(card);
+    col3 = appendChild(card);
+    row  = appendChild(col1);
+    row  = appendChild(col2);
+    row  = appendChild(col3);
+}
+
+const renderElements = () => {
+    const cardsContainer = document.getElementById(render);
+    const container = contentCards();
+    cardsContainer.appendChild(container);
+}
+
+renderElements();
 view.testMethod();
