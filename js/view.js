238c173122ed54b5ec3b701ec07b55155cf74722
Luis Cazzullo <lcazzullo@gmail.com>
2019-10-24 23:03:06 -0300

    Function showModal and FontAwesome link were added

--- js/view.js
+++ js/view.js
@@ -36,27 +36,61 @@ const view = (shop => {
         }
         return element;
     }
+    // This function create a modal window with Bootstrap Classes
     const createModal = () => {
         const _modalRow = createElement('div', '', 'row', false, '', '');
         const _modalCol = createElement('div', '', 'col-md-12', false, '', '');
+        const openBtn = createElement('button', 'openBtn', 'btn btn-primary', false, '', 'Open Modal')
         const _modalBox = createElement('div', 'modalBox', 'modal fade', false, '', '');
-        const _modalDialog = createElement('div', '', 'modal-dialog', false, '', '');
+        const _modalDialog = createElement('div', '', 'modal-dialog modal-dialog-centered modal-lg', false, '', '');
         const _modalContent = createElement('div', '', 'modal-content', false, '', '');
-        const _modalHeader= createElement('div', '', 'modal-header', false, '', '');
+        const _modalHeader = createElement('div', 'modalHeader', 'modal-header', false, '', '');
+        const _modalBody = createElement('div', 'modalBody', 'modal-body', false, '', '');
+        const _modalFooter = createElement('div', 'modalBody', 'modal-footer', false, '', '');
+        const _modalIcon = createElement('i', 'modalIcon', '', false, '', '');
+        const _modalMessage = createElement('h3', 'modalMessage', 'text-justify, font-weight-bold', false, '', '');
+        const closeModal = createElement('button', 'modalBtn', 'btn btn-danger', false, '', 'Continue');
+        closeModal.type = 'button';
+        closeModal.setAttribute('data-dismiss', 'modal');
         _modalContainer.appendChild(_modalRow);
         _modalRow.appendChild(_modalCol);
         _modalCol.appendChild(_modalBox);
         _modalBox.appendChild(_modalDialog);
         _modalDialog.appendChild(_modalContent);
         _modalContent.appendChild(_modalHeader);
+        _modalContent.appendChild(_modalBody);
+        _modalContent.appendChild(_modalFooter);
+        _modalHeader.appendChild(_modalIcon);
+        _modalBody.appendChild(_modalMessage);
+        _modalFooter.appendChild(closeModal);
+        return _modalRow;
     }
+
+    const showModal = (success, error) => {
+        const modal = createModal();
+        const _icon = document.getElementById('modalIcon');
+        const _modalMessage = document.getElementById('modalMessage');
+        const _modalBtn = document.getElementById('modalBtn');
+        if (success) {
+            _icon.classList.add('fas', 'fa-check-circle');
+            _modalMessage.classList.add('text-success');
+            _modalMessage.textContent = 'Su compra ha sido un éxito';
+            _modalBtn.textContent = 'Continue';
+        } else {
+            _modalMessage.classList.add(<'fas', 'fa-exclamation-triangle');
+            _modalMessage.classList.add('text-warning');
+            _modalMessage.textContent = 'Ocurrió un error, intente de nuevo.';
+            _modalBtn.textContent = 'Intente de Nuevo';
+        }
+    } 
 
     return {
         testMethod,
         createModal,
-        createElement
+        createElement,
+        showModal
     }
 })(shop);
 
 view.testMethod();
-view.createModal();
+view.showModal();
