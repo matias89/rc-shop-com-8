0447d18684ab5222a5042cca2616b112d0ea52f6
Romina <romy_agus_14@hotmail.com>
2019-10-26 18:49:49 -0300

    task_9

--- js/shop.js
+++ js/shop.js
@@ -20,12 +20,44 @@ const shop = (() => {
     const testMethod = () => {
         return 'Hello World!';
     }
+    const addProduct = (product) => {
+        const products = product;
+            if(products){
+                setItem("products",products);
+            } else {
+                console.log("No hay productos")
+            }  
+    }
+    const removeProduct = () => {
+        localStorage.removeItem(key);
+    }
+    const view = (prod) => {
+       
+    }
+
+    const setItem = (key,valor) => {
+        localStorage.setItem(key,JSON.stringify(valor));
+        console.log("key",valor);
+    }
+
+    const getItem = (key) =>{
+        localStorage.getItem(JSON.parse(key));
+    }
     return {
         getProduct,
         getProducts,
+        addProduct,
         testMethod
     }
 })();
 
 shop.getProducts();
 shop.getProduct(3);
+shop.addProduct();
+  
+
+ 
+
+
+   
+
