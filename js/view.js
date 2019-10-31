54803895867b9e9d2737172cac4b37738cbbfe74
Ramiro Gisone <ramirogisone@gmail.com>
2019-10-31 18:47:52 -0300

    subo estructura para creacion de cards (#4)
    * subo estructura para creacion de cards
    
    * agrego funciones para creacion de cards
    
    * agrego funciones al modulo ppal.

--- js/view.js
+++ js/view.js
@@ -25,8 +25,11 @@ const view = (shop => {
         if (elementType === 'input') {
             element.placeholder = placeholder ;
             element.id = id;
+        } 
+        if (htmlFor) {
+            element.htmlFor = htmlFor;
         } else if (elementType === 'label') {
-            element.for = id;
+            element.For = id;
         }
         if (elementType === 'img') {
             element.src = content;
@@ -37,7 +40,24 @@ const view = (shop => {
         }
         return element;
     }
-
+    const createCard = () => {
+        const cardDeck = createElement('div', '', 'card-deck', false, false, false, false);;
+        const card  = createElement('div', '', 'card', false, false, false, false);
+        const img   = createElement('img', '', 'card-img-top',false, false, false, 'https://i1.wp.com/www.sopitas.com/wp-content/uploads/2019/01/boo-perrito-lindo-1120x581.jpeg');
+        const cardB = createElement('div', '', 'card-body', false, false, false, false);
+        const title = createElement('h5', '', 'card-title', false, false, 'Esto es un titulo', false);
+        const text  = createElement('p','','card-text', false, false, 'Esto es un texto', false);
+        const textS = createElement('p', '', 'text-muted small', false, false, 'Esto es un texto small', false);
+        card.appendChild(img);
+        card.appendChild(cardB);
+        cardB.appendChild(title);
+        cardB.appendChild(text);
+        cardB.appendChild(textS);
+        cardDeck.appendChild(card);
+        const cardId =  document.getElementById('cardId');
+        cardId.appendChild(cardDeck);
+        return cardDeck
+    }
     const createItem = () => {
         const divContainer = createElement('div', false, 'carousel-item');
         const img = createElement('img', false, 'd-block w-100', false, false, 'https://fravega.vteximg.com.br/arquivos/ids/6191779-1000-1000/celular-liberado-samsung-galaxy-s10e-azul-781304.jpg');
@@ -50,18 +70,15 @@ const view = (shop => {
         divContainer.appendChild(paragraph);
         return divContainer;
     }
-
     const buildCarouselItems = () => {
         const renderArea = document.getElementById('productdetail');
         const item = createItem();
         renderArea.appendChild(item);
     }
-
-
     return {
-        testMethod,
         createElement,
-        buildCarouselItems
+        buildCarouselItems,
+        createCard
     }
 })(shop);
 
