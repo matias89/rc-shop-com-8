83ca058912b29de0274d46596a853c43cab72aed
Romi91 <53384489+Romi91@users.noreply.github.com>
2019-10-31 18:49:18 -0300

    9- create-cart-behavior (#13)
    * task_9
    
    * task_9
    
    * Task_9
    
    * Task_9

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,5 @@
 const shop = (() => {
+    const shopKey = 'products';
     const getProducts = () => {
         fetch('http://localhost:3000/products')
             .then(response => {
@@ -20,12 +21,73 @@ const shop = (() => {
     const testMethod = () => {
         return 'Hello World!';
     }
+    const addProduct = (product) => {
+        product.cant = 1; 
+        const products = getItem(shopKey);
+            if (products) {
+                products.push(product);
+                setItem(shopKey, products);
+            } else {
+                setItem(shopKey, [product]);
+            }  
+    }
+    const removeProduct = (id) => {
+        const products = getItem(shopKey);
+        let c;
+        for(let i = 0; i < products.length ; i++){
+            if (products[i].id === id) {
+                c = i;
+            }
+        }
+        console.log(products);
+        if (c) {
+            products.splice(c, 1);
+        }
+        console.log(products);
+        setItem(shopKey, products);
+    }
+    const updateProduct = (id,j) => {
+        const products = getItem(shopKey);
+        for( let i = 0; i < products.length; i++){
+            if(products[i].id === id ){
+                products[i].cant = j;
+            }
+        }
+        setItem(shopKey,products);
+        console.log(products);
+   }
+   const clearproducts = () => { 
+        const products = getItem(shopKey);
+        if(products) {
+          localStorage.clear(products);
+        }
+   }
+    const confirmProduct = () => {
+        return getItem(shopKey);
+    }
+    const setItem = (key,valor) => {
+        localStorage.setItem(key,JSON.stringify(valor));
+    }
+    const getItem = (key) => {
+        return JSON.parse(localStorage.getItem(key));
+    }
     return {
         getProduct,
         getProducts,
+        addProduct,
+        removeProduct,
+        updateProduct,
+        clearproducts,
+        confirmProduct,
         testMethod
     }
 })();
-
 shop.getProducts();
 shop.getProduct(3);
+
+
+ 
+
+
+   
+
