7fe0077b5a96e090b15ee1188363716ee956280f
luiscazzullo <47572807+luiscazzullo@users.noreply.github.com>
2019-11-01 12:14:13 -0300

    8 json products (#14)
    * Incomplete JSON and Images were added
    
    * WIP-folder images were modified
    
    * Images Folder was changed to products and the JSON was completed
    
    * underscore were added
    
    * properties were fixed
    
    * underscores were added
    
    * WIP: createModal
    
    * WIP-Function createModal was improved
    
    * Function showModal and FontAwesome link were added
    
    * Promise was added
    
    * fixed some errors
    
    * Details were modified
    
    * errors and conflicts added

--- js/view.js
+++ js/view.js
@@ -176,6 +176,72 @@ const view = (shop => {
         //cardId.appendChild(cardDeck);
         return cardDeck
     }
+    // This function create a modal window with Bootstrap Classes
+    const createModal = () => {
+        const _modalRow = createElement('div', '', 'row', false);
+        const _modalCol = createElement('div', '', 'col-md-12', false);
+        const _openBtn = createElement('button', 'openBtn', 'btn btn-primary', false, false, 'Open Modal')
+        const _modalBox = createElement('div', 'modalBox', 'modal fade');
+        const _modalDialog = createElement('div', '', 'modal-dialog modal-dialog-centered modal-lg');
+        const _modalContent = createElement('div', '', 'modal-content');
+        const _modalHeader = createElement('div', 'modalHeader', 'modal-header');
+        const _modalBody = createElement('div', 'modalBody', 'modal-body');
+        const _modalFooter = createElement('div', 'modalBody', 'modal-footer');
+        const _modalIcon = createElement('i', 'modalIcon', '');
+        const _modalMessage = createElement('h3', 'modalMessage', 'text-justify, font-weight-bold', );
+        const closeModal = createElement('button', 'modalBtn', 'btn btn-danger', false, false, 'Continue');
+        closeModal.type = 'button';
+        closeModal.setAttribute('data-dismiss', 'modal');
+        _modalContainer.appendChild(_modalRow);
+        _modalRow.appendChild(_modalCol);
+        _modalCol.appendChild(_modalBox);
+        _modalRow.appendChild(_openBtn);
+        _modalBox.appendChild(_modalDialog);
+        _modalDialog.appendChild(_modalContent);
+        _modalContent.appendChild(_modalHeader);
+        _modalContent.appendChild(_modalBody);
+        _modalContent.appendChild(_modalFooter);
+        _modalHeader.appendChild(_modalIcon);
+        _modalBody.appendChild(_modalMessage);
+        _modalFooter.appendChild(closeModal);
+        return _modalRow;
+    }
+    //This function modified content inside the modal
+    const showModal = (success, error) => {
+        const modal = createModal();
+        const _icon = document.getElementById('modalIcon');
+        const _modalMessage = document.getElementById('modalMessage');
+        const _modalBtn = document.getElementById('modalBtn');
+        if (success) {
+            _icon.classList.add('fas', 'fa-check-circle');
+            _modalMessage.classList.add('text-success');
+            _modalMessage.textContent = 'Su compra ha sido un éxito';
+            _modalBtn.textContent = 'Continue';
+        } 
+        if (error) {
+            _modalMessage.classList.add('fas', 'fa-exclamation-triangle');
+            _modalMessage.classList.add('text-warning');
+            _modalMessage.textContent = 'Ocurrió un error, intente de nuevo.';
+            _modalBtn.textContent = 'Intente de Nuevo';
+        }
+        return modal;
+    }
+    // Promise to simulate buying process
+    const openModal = new Promise((resolve, reject) => {
+        let num = Math.random();
+        if (num > 0.5) {
+            resolve();
+        } else {
+            reject(error);
+        }
+    })
+    openModal
+        .then(()=> {
+            showModal(success);
+        })
+        .catch((error)=> {
+            showModal(error);
+        })
     return {
         createModal,
         createElement,
@@ -186,15 +252,6 @@ const view = (shop => {
         createProductsRow
     }
 })(shop);
-/*
-view.createSpinner();
-view.buildCarouselItems();
-view.openModal()
-        .then(()=> {
-            view.showModal(true);
-        })
-        .catch(()=> {
-            view.showModal(false);
-        })
-*/
 
+view.showModal();
+view.testMethod();
