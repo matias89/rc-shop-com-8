eaa2adcd9ea4bded0959a5e65df4e42e422b2ea1
luiscazzullo <47572807+luiscazzullo@users.noreply.github.com>
2019-11-02 20:47:08 -0300

    14 buying process (#15)
    * Incomplete JSON and Images were added
    
    * WIP-folder images were modified
    
    * Images Folder was changed to products and the JSON was completed
    
    * underscore were added
    
    * properties were fixed
    
    * underscores were added
    
    * WIP: createModal
    
    * WIP-Function createModal was improved
    
    * Function showModal and FontAwesome link were added
    
    * Promise was added
    
    * fixed some errors
    
    * conflicts solved in HTML and promise OpenModal fixed
    
    * fixed promise and showModal function
    
    * WIP with the spinner. I have problems with the validate function
    
    * Promise includes spinner
    
    * Images replaces icons. Error and Success images were added
    
    * conflicts in view.js were solved

--- js/view.js
+++ js/view.js
@@ -41,52 +41,47 @@ const view = (shop => {
     const createModal = () => {
         const _modalRow = createElement('div', '', 'row', false);
         const _modalCol = createElement('div', '', 'col-md-12', false);
-        const _openBtn = createElement('button', 'openBtn', 'btn btn-primary', false, false, 'Open Modal');
         const _modalBox = createElement('div', 'modalBox', 'modal fade');
         const _modalDialog = createElement('div', '', 'modal-dialog modal-dialog-centered modal-lg');
         const _modalContent = createElement('div', '', 'modal-content');
         const _modalHeader = createElement('div', 'modalHeader', 'modal-header');
         const _modalBody = createElement('div', 'modalBody', 'modal-body');
-        const _modalFooter = createElement('div', 'modalBody', 'modal-footer');
-        const _modalIcon = createElement('i', 'modalIcon', '');
+        const _modalFooter = createElement('div', 'modalFooter', 'modal-footer');
+        const _modalImage = createElement('img', 'modalImage', 'w-50 m-auto');
         const _modalMessage = createElement('h3', 'modalMessage', 'text-justify, font-weight-bold', );
         const closeModal = createElement('button', 'modalBtn', 'btn btn-danger close', false, false, 'Continue');
-        _openBtn.setAttribute('data-toggle', 'modal')
-        _openBtn.setAttribute('data-target', '#modalBox')
         closeModal.type = 'button';
         closeModal.setAttribute('data-dismiss', 'modal');
         if (_modalContainer) {
-            _modalContainer.appendChild(_modalRow);
-        }
+        _modalContainer.appendChild(_modalRow);
         _modalRow.appendChild(_modalCol);
-        _modalCol.appendChild(_openBtn);
         _modalCol.appendChild(_modalBox);
-        _modalBox.appendChild(_modalDialog);
-        _modalDialog.appendChild(_modalContent);
+        _modalBox.appendChild(_modalDialog)
         _modalContent.appendChild(_modalHeader);
         _modalContent.appendChild(_modalBody);
         _modalContent.appendChild(_modalFooter);
-        _modalHeader.appendChild(_modalIcon);
+        _modalHeader.appendChild(_modalImage);
         _modalBody.appendChild(_modalMessage);
         _modalFooter.appendChild(closeModal);
+        }
         return _modalRow;
     }
     //This function modified content inside the modal
     const showModal = status => {
         const modal = createModal();
-        const _icon = document.getElementById('modalIcon');
+        const _modalImage = document.getElementById('modalImage');
         const _modalMessage = document.getElementById('modalMessage');
         const _modalBtn = document.getElementById('modalBtn');
         if (status) {
-            _icon.classList.add('fas', 'fa-check-circle');
             if (_modalMessage) {
+                _modalImage.src = './img/success.png';
                 _modalMessage.classList.add('text-success');
                 _modalMessage.innerHTML = 'Su compra ha sido un éxito';
             }
             _modalBtn.textContent = 'Continue';
         } else {
             if (_modalMessage) {
-                _modalMessage.classList.add('fas', 'fa-exclamation-triangle');
+                _modalImage.src = './img/error.png';
                 _modalMessage.classList.add('text-danger');
                 _modalMessage.innerHTML = 'Ocurrió un error, intente de nuevo.';
             }
@@ -101,25 +96,29 @@ const view = (shop => {
         }
         return _spinner
     }
-    // Promise to simulate buying process
-    const openModal = () => {
-        return new Promise((resolve, reject) => {
-            const num = (new Date()).toLocaleTimeString().substr(-1);
-            console.log(num)
-            if (num > 5) {
-                resolve();
-            } else {
-                reject(error);
-            }
-        })
-    }
     const showSpinner = () => {
         const _spinner = document.getElementById('spinner');
         _spinner.classList.remove('d-none');
     }
-    const validate = () => {
-        setTimeout(showSpinner(), 1500);
+    const hideSpinner = () => {
+        const _spinner = document.getElementById('spinner');
+        _spinner.classList.add('d-none');
     }
+// Promise to simulate buying process
+const toggleModal = () => {
+    showSpinner();
+    return new Promise((resolve, reject) => {
+        const num = (new Date()).toLocaleTimeString().substr(-1);
+        setTimeout(() => {
+            hideSpinner();
+        }, 1500)
+        if (num > 5) {
+            resolve();
+        } else {
+            reject(error);
+        }
+    })
+}
     const createItem = () => {
         const divContainer = createElement('div', false, 'carousel-item');
         const img = createElement('img', false, 'd-block w-100', false, false, 'https://fravega.vteximg.com.br/arquivos/ids/6191779-1000-1000/celular-liberado-samsung-galaxy-s10e-azul-781304.jpg');
@@ -150,7 +149,6 @@ const view = (shop => {
         }
         el.appendChild(row);
     }
-
     const createCard = (cardTitle, body, price, images, id) => {
         let cardImg = './images/default-img.jpg';
         if (images.length) {
@@ -176,77 +174,11 @@ const view = (shop => {
         //cardId.appendChild(cardDeck);
         return cardDeck
     }
-    // This function create a modal window with Bootstrap Classes
-    const createModal = () => {
-        const _modalRow = createElement('div', '', 'row', false);
-        const _modalCol = createElement('div', '', 'col-md-12', false);
-        const _openBtn = createElement('button', 'openBtn', 'btn btn-primary', false, false, 'Open Modal')
-        const _modalBox = createElement('div', 'modalBox', 'modal fade');
-        const _modalDialog = createElement('div', '', 'modal-dialog modal-dialog-centered modal-lg');
-        const _modalContent = createElement('div', '', 'modal-content');
-        const _modalHeader = createElement('div', 'modalHeader', 'modal-header');
-        const _modalBody = createElement('div', 'modalBody', 'modal-body');
-        const _modalFooter = createElement('div', 'modalBody', 'modal-footer');
-        const _modalIcon = createElement('i', 'modalIcon', '');
-        const _modalMessage = createElement('h3', 'modalMessage', 'text-justify, font-weight-bold', );
-        const closeModal = createElement('button', 'modalBtn', 'btn btn-danger', false, false, 'Continue');
-        closeModal.type = 'button';
-        closeModal.setAttribute('data-dismiss', 'modal');
-        _modalContainer.appendChild(_modalRow);
-        _modalRow.appendChild(_modalCol);
-        _modalCol.appendChild(_modalBox);
-        _modalRow.appendChild(_openBtn);
-        _modalBox.appendChild(_modalDialog);
-        _modalDialog.appendChild(_modalContent);
-        _modalContent.appendChild(_modalHeader);
-        _modalContent.appendChild(_modalBody);
-        _modalContent.appendChild(_modalFooter);
-        _modalHeader.appendChild(_modalIcon);
-        _modalBody.appendChild(_modalMessage);
-        _modalFooter.appendChild(closeModal);
-        return _modalRow;
-    }
-    //This function modified content inside the modal
-    const showModal = (success, error) => {
-        const modal = createModal();
-        const _icon = document.getElementById('modalIcon');
-        const _modalMessage = document.getElementById('modalMessage');
-        const _modalBtn = document.getElementById('modalBtn');
-        if (success) {
-            _icon.classList.add('fas', 'fa-check-circle');
-            _modalMessage.classList.add('text-success');
-            _modalMessage.textContent = 'Su compra ha sido un éxito';
-            _modalBtn.textContent = 'Continue';
-        } 
-        if (error) {
-            _modalMessage.classList.add('fas', 'fa-exclamation-triangle');
-            _modalMessage.classList.add('text-warning');
-            _modalMessage.textContent = 'Ocurrió un error, intente de nuevo.';
-            _modalBtn.textContent = 'Intente de Nuevo';
-        }
-        return modal;
-    }
-    // Promise to simulate buying process
-    const openModal = new Promise((resolve, reject) => {
-        let num = Math.random();
-        if (num > 0.5) {
-            resolve();
-        } else {
-            reject(error);
-        }
-    })
-    openModal
-        .then(()=> {
-            showModal(success);
-        })
-        .catch((error)=> {
-            showModal(error);
-        })
     return {
         createModal,
         createElement,
+        toggleModal,
         showModal,
-        openModal,
         buildCarouselItems,
         createSpinner,
         createProductsRow
